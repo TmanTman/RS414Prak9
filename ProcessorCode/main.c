c6e1b6ded2a734650863042994665f4a1eeaae9c
Tielman Nieuwoudt <16082370@sun.ac.za>
2013-05-08 22:12:59 +0200

    loop through whole grid

--- ProcessorCode/main.c
+++ ProcessorCode/main.c
@@ -7,16 +7,25 @@ int tap[3] = {2, 4, 3};
 __asm void firRow(void)
 {
 	//Initialize 
-	mov r1, #0; counter
+	mov r1, #0; column counter
+	mov r10, #0; row counter
 	;Load gain values
  	ldr r12, =__cpp(&tap);
  	ldr r7, [r12];
  	ldr r6, [r12,#4];
 	ldr r5, [r12,#8];
 	ldr r12, =__cpp(&grid); pointer to grid start address
-colStart
-	cmp r1, #4;
-	beq done;
+	//Outer Loop
+newRow
+	//Reset column counter
+	mov r1, #0;
+	//Check whether last row has been reached
+	cmp r10, #4; 
+	beq gridDone;
+	//Inner Loop
+colStart ; Inner loop
+	cmp r1, #4; Test whether end of column has been reached
+	beq rowDone;
 	//Load row values
 	cmp r1, #0; //special load operation at start of row
 	beq counter0 ;
@@ -54,7 +63,11 @@ next
 	add r1, #1;
 	b colStart
 	//return to main
-done	
+rowDone
+	add r10, #1; increase row counter
+	add r12, #4; shift r12 to beginning of next row
+	b newRow
+gridDone	
 	bx lr;
 }
 
